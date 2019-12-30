4221181cfec9256a8413d4d80801bfaf9b385c72
fankaifun4 <fankaifun4@qq.com>
2019-12-30 11:18:45 +0800

    no message

--- src/config/http.config.js
+++ src/config/http.config.js
@@ -1,8 +1,8 @@
 let baseUrl=''
 if (process.env.NODE_ENV === 'development') {
-  baseUrl = 'http://localhost:7001'
+  baseUrl = 'http://localhost:7001/999lp'
 } else if (process.env.NODE_ENV === 'production') {
-  baseUrl = ''
+  baseUrl = 'https://www.999lp.cn/fkx'
 }
 export default {
   baseUrl
